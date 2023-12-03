612d788c3dfdd534575bbd7c6d1d9d50e106ae06
thisara-medi <thisarabandara.medi@gmail.com>
2023-12-04 00:02:24 +0530

    add updated video

--- src/views/mobileView/MobileView.tsx
+++ src/views/mobileView/MobileView.tsx
@@ -64,8 +64,8 @@ const MobileView = () => {
 							// onEnded={onVideoEnded}
 							// controls
 						>
-							<source src="/demo.mp4" type="video/mp4" />
-							<source src="demo.webm" type="video/webm" />
+							<source src="/CT V_1.2.mp4" type="video/mp4" />
+							<source src="CT V_1.2.webm" type="video/webm" />
 							Your browser does not support the video tag.
 						</video>
 					</div>
