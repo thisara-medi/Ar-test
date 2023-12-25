94de00c5b1eb8d28caacdd5ed2f3f8104fc9b8b5
thisara-medi <thisarabandara.medi@gmail.com>
2023-12-25 16:08:59 +0530

    add sephora

--- src/views/mobileView/MobileView.tsx
+++ src/views/mobileView/MobileView.tsx
@@ -44,7 +44,7 @@ const MobileView = () => {
 					<div className="relative">
 						{isPlaying && (
 							<a
-								href={isPlaying ? "/GiftBox2.usdz" : "#"}
+								href={isPlaying ? "/SEPHORA.usdz" : "#"}
 								rel="ar"
 								className="block h-[100%] w-[100%] absolute z-[1000]"
 							>
