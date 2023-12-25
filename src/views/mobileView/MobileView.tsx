d9eba1e244e8ae996c534fffc08493dd06d9c5f5
thisara-medi <thisarabandara.medi@gmail.com>
2023-12-25 16:16:26 +0530

    add toyota

--- src/views/mobileView/MobileView.tsx
+++ src/views/mobileView/MobileView.tsx
@@ -44,7 +44,7 @@ const MobileView = () => {
 					<div className="relative">
 						{isPlaying && (
 							<a
-								href={isPlaying ? "/SEPHORA.usdz" : "#"}
+								href={isPlaying ? "/toyota.usdz" : "#"}
 								rel="ar"
 								className="block h-[100%] w-[100%] absolute z-[1000]"
 							>
